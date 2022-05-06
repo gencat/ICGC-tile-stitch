8f5544f75366bdd9ea798315fd9c096f413b9927
Fran Martin <francisco.martin@icgc.cat>
2022-05-06 13:47:54 +0000

    feat(zoom-6): add zoom 6 to the Tile stitch

--- config.js
+++ config.js
@@ -9,16 +9,12 @@ if (_platform.indexOf('Windows') != -1) {
 var _f = JSON.parse(fs.readFileSync(fileProp, 'utf8'));
 
 _f_origen_comprimid= true;
-_f.mbtiles_ori2 = './ctxmaps_20210720.mbtiles';
-_f.mbtiles_ori3 = './2017-07-03_europe_spain.mbtiles';
-_f.mbtiles_ori = './data/contextmaps.mbtiles';
+_f.mbtiles_ori = './data/contextmaps_cat.mbtiles';
 _f.mbtiles_dest = './data/europe_spain.mbtiles';
-_f.pol_cat = './data/catalunya.geojson';
+_f.pol_cat = './CAT_plus_box.geojson';
 _f.mbtiles_dest_source = './europe_spain_Copy.mbtiles';
 _f.mbtiles_planet = './planet14.mbtiles';
-_f.zoom_levels = [7,8,9,10,11,12,13,14];
-//_f.zoom_levels = [15,16];
+_f.zoom_levels = [6,7,8,9,10,11,12,13,14];
 _f.zoom_levels_extra = [10,11,12,13];
-//_f.zoom_levels = [7];
 
 module.exports = _f;
